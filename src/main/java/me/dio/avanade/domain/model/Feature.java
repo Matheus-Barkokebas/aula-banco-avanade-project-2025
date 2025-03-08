eaf53fe7a5e0e2dce954d85478292c7f750f2247
Matheus <matheusbarkcaribe@gmail.com>
2025-03-08 15:36:46 -0300

    mapeamento realcional, JPA

---
+++ src/main/java/me/dio/avanade/domain/model/Feature.java
@@ -0,0 +1,8 @@
+package me.dio.avanade.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+
+}
---
+++ src/main/java/me/dio/avanade/domain/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package me.dio.avanade.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import me.dio.avanade.domain.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
