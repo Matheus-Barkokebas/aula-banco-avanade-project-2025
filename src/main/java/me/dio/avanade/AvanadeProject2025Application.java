ef01d3748e2a1ec23a01a7b8ac8c90e626467dc0
Matheus <matheusbarkcaribe@gmail.com>
2025-03-08 14:18:34 -0300

    initial commit

---
+++ src/main/java/me/dio/avanade/AvanadeProject2025Application.java
@@ -0,0 +1,13 @@
+package me.dio.avanade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvanadeProject2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvanadeProject2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/avanade/AvanadeProject2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.avanade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvanadeProject2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
